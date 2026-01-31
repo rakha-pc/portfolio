1f5a5f4938aee6338b673b0c649c59d13d065f4f
rakha-pc <rakhahanief@gmail.com>
2026-01-31 18:43:59 +0700

    ubah env

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,6 @@ export default defineConfig(({ mode }) => ({
       }),
     }),
   ],
-  base: mode === "gh-pages" ? "/portofolio/" : "/",
+  base: mode === "gh-pages" ? "/portfolio/" : "/",
   
 }));
